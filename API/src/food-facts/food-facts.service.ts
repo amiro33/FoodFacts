7c6c3981c19dd6efc0002cc3fe5be24a50f04b23
jose <josersanchez0117@gmail.com>
2025-02-03 10:14:06 -0600

    food facts

--- API/src/food-facts/food-facts.service.ts
+++ API/src/food-facts/food-facts.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class FoodFactsService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Food Facts GO HERE!';
   }
 }
