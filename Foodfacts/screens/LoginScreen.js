87851545ad72f369348202c5b2b600e7238a81a6
A Miro <82675315+amiro33@users.noreply.github.com>
2025-03-03 17:44:43 -0600

    idk

--- Foodfacts/screens/LoginScreen.js
+++ Foodfacts/screens/LoginScreen.js
@@ -1,9 +1,5 @@
-
-import { useContext, useState } from 'react';
-import { View, Button, StyleSheet, Text, TextInput, Image, Alert } from 'react-native';
-import { AuthContext } from '../context/AuthContext';
-
-import foodLogo from '../assets/Foodfactslogo.png';  
+/* import { useState } from 'react';
+import { View, Button, Image, StyleSheet, Text, TextInput } from 'react-native';
 
 export const LoginScreen = () => {
     const authContext = useContext(AuthContext);
@@ -109,5 +105,73 @@ export const LoginScreen = () => {
                 </View>
             </View>
         </View>
-    );
-};
+    )
+} */ 
+
+import { useContext, useState } from 'react';
+import { View, Button, Image, StyleSheet, Text, TextInput } from 'react-native';
+import { AuthContext } from '../context/AuthContext';
+
+export const LoginScreen = () => {
+    const authContext = useContext(AuthContext);
+   const createAccount = async () => {
+        const req = await fetch('http://3.17.79.194:3000/auth/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+        const res = await req.json();
+        if (res.success) {
+            authContext.login(res.user);
+        }
+    };
+
+    const logIn = async () => {
+        const req = await fetch('http://3.17.79.194:3000/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+        const res = await req.json();
+
+        if (res.access_token) {
+            authContext.login({ username, token: res.access_token });
+        }
+    };
+
+
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const styles = StyleSheet.create({
+        container: {
+            paddingY: 16,
+            paddingX: 4
+        },
+        input: {
+          height: 40,
+          borderWidth: 1,
+          padding: 10,
+        },
+        buttonContainer: {
+            display: 'flex',
+            flexDirection: 'row'
+        }
+      });
+
+    return(
+        <View style={styles.container}>
+            <Text>Log In to App</Text>
+            <TextInput style={styles.input} value={username} onChange={(value) => setUsername(value)}></TextInput>
+            <TextInput style={styles.input} value={password} onChange={(value) => setPassword(value)}></TextInput>
+            <View style={styles.buttonContainer}>
+            <Button title="Log In" />
+            <Button title="Create Account" />
+            </View>
+        </View>
+    )
+}
