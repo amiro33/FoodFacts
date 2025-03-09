02434559e7d52f0d21bb05ffa7d8bb7be43a08a1
jose <josersanchez0117@gmail.com>
2025-03-09 15:05:26 -0500

    actual fixes lol

--- API/src/entities/user.entity.ts
+++ API/src/entities/user.entity.ts
@@ -1,17 +1,16 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column('text')
-    username: string;
+  @Column('text')
+  username: string;
 
-    @Column('text')
-    password: string;
+  @Column('text', { select: false })
+  password: string;
 }
-
 
 import { DataSource } from 'typeorm';
 
--- API/src/users/users.controller.ts
+++ API/src/users/users.controller.ts
@@ -1,13 +1,7 @@
-import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
-import { UsersService } from "./users.service";
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {
-
-    }
-    @Get(':id') 
-    async getUser(@Param('id', ParseIntPipe) id: number) {
-        return await this.usersService.findOneById(id);
-    }
+  constructor(private readonly usersService: UsersService) {}
 }
